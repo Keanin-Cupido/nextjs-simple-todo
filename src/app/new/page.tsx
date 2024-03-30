1621c2345b6a82e4ef7b8512c910465680ed5af8
Keanin Cupido <keanin16@gmail.com>
2024-03-30 22:55:10 +0200

    create todo page

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,18 @@
+import Link from "next/link";
+
 export default function Page(){
-    return <h1>New</h1>
+    return (
+        <>
+          <header className="flex justify-between items-center mb-4">
+            <h1 className="text-2xl">New Todo</h1>
+          </header>
+          <form className="flex gap-2 flex-col">
+            <input type="text" name="title" className="border border-green-300 bg-transparent px-2 py-1 outline-none focus-within:border-green-100" />
+            <div className="flex gap-1 justify-end">
+                <Link href=".." className="border border-green-300 bg-transparent px-2 py-1 outline-none hover:bg-green-900 focus-within:border-green-100">Cancel</Link>
+                <button type="submit" className="border border-green-300 bg-transparent px-2 py-1 outline-none hover:bg-green-900 focus-within:border-green-100">Create</button>
+            </div>
+          </form>
+        </>
+      )
 }
