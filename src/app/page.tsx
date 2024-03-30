84ff8ee476e04e39e42320d986b75108bc66e90d
Keanin Cupido <keanin16@gmail.com>
2024-03-30 22:37:10 +0200

    create todoitem component & data fetching function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
-export default function Home () {
-  return <h1>Hi</h1>
+import { prisma } from "@/db";
+import Link from "next/link";
+import { TodoItem } from "@/components/TodoItem";
+
+function getTodos() {
+  return prisma.todo.findMany();
+}
+
+export default async function Home () {
+  const todos = await getTodos();
+  // await prisma.todo.create({data: {title: "Test Todo", complete: false}});
+
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Todos</h1>
+        <Link href='/new' className="border-b-2 px-4 py-1 border-green-100 transition-all hover:bg-green-800 focus-within:bg-green-800 outline-none">New</Link>
+      </header>
+      <ul>
+        {todos.map(todo => (
+          <TodoItem key={todo.id} {...todo} />
+        ))}
+      </ul>
+    </>
+  )
 }
