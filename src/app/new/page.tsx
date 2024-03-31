4f91b43b89942aea07a0a3bd47678a389f6c9447
Keanin Cupido <keanin16@gmail.com>
2024-03-31 12:19:45 +0200

    feat(forms): server actions

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,4 +1,19 @@
+import { prisma } from "@/db";
 import Link from "next/link";
+import { redirect } from "next/navigation";
+
+async function createTodo(data: FormData) {
+    "use server"
+
+    const title = data.get("title")?.valueOf();
+    if (typeof title !== "string" || title.length === 0) {
+        throw new Error("Invalid Title");
+    }
+
+    await prisma.todo.create({data: {title, complete: false}});
+
+    redirect("/");
+}
 
 export default function Page(){
     return (
@@ -6,7 +21,7 @@ export default function Page(){
           <header className="flex justify-between items-center mb-4">
             <h1 className="text-2xl">New Todo</h1>
           </header>
-          <form className="flex gap-2 flex-col">
+          <form action={createTodo} className="flex gap-2 flex-col">
             <input type="text" name="title" className="border border-green-300 bg-transparent px-2 py-1 outline-none focus-within:border-green-100" />
             <div className="flex gap-1 justify-end">
                 <Link href=".." className="border border-green-300 bg-transparent px-2 py-1 outline-none hover:bg-green-900 focus-within:border-green-100">Cancel</Link>
